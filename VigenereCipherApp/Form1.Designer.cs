086d717f0dad35e394cdbc3506e4b926947798d5
TRIGON666 <jakeggwp@gmail.com>
2025-04-10 03:42:44 +0300

    Добавьте файлы проекта.

---
+++ VigenereCipherApp/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace VigenereCipherApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.enc = new System.Windows.Forms.Button();
+            this.dec = new System.Windows.Forms.Button();
+            this.tKey = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 55);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(404, 134);
+            this.txtInput.TabIndex = 0;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(12, 225);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(404, 134);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Исходный текст:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Результат:";
+            // 
+            // enc
+            // 
+            this.enc.Location = new System.Drawing.Point(15, 415);
+            this.enc.Name = "enc";
+            this.enc.Size = new System.Drawing.Size(144, 23);
+            this.enc.TabIndex = 4;
+            this.enc.Text = "Зашифровать";
+            this.enc.UseVisualStyleBackColor = true;
+            this.enc.Click += new System.EventHandler(this.enc_Click);
+            // 
+            // dec
+            // 
+            this.dec.Location = new System.Drawing.Point(165, 415);
+            this.dec.Name = "dec";
+            this.dec.Size = new System.Drawing.Size(144, 23);
+            this.dec.TabIndex = 5;
+            this.dec.Text = "Расшифровать";
+            this.dec.UseVisualStyleBackColor = true;
+            this.dec.Click += new System.EventHandler(this.dec_Click);
+            // 
+            // tKey
+            // 
+            this.tKey.Location = new System.Drawing.Point(422, 55);
+            this.tKey.Multiline = true;
+            this.tKey.Name = "tKey";
+            this.tKey.Size = new System.Drawing.Size(134, 25);
+            this.tKey.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(419, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Ключ:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(422, 104);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(134, 21);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(422, 88);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Язык:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(607, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tKey);
+            this.Controls.Add(this.dec);
+            this.Controls.Add(this.enc);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button enc;
+        private System.Windows.Forms.Button dec;
+        private System.Windows.Forms.TextBox tKey;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ VigenereCipherApp/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VigenereCipherApp
+{
+    public partial class Form1 : Form
+    {
+        private const string RussianLetters = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+        private const string EnglishLetters = "abcdefghijklmnopqrstuvwxyz";
+
+        private const string RussianAlphabet = RussianLetters + "0123456789";
+        private const string EnglishAlphabet = EnglishLetters + "0123456789";
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetupUI();
+        }
+
+        private void SetupUI()
+        {
+            txtOutput.ReadOnly = true;
+            txtOutput.ScrollBars = ScrollBars.Vertical;
+            txtInput.ScrollBars = ScrollBars.Vertical;
+            
+            comboBox1.Items.AddRange(new[] { "Русский", "Английский" });
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private string ProcessVigenere(string text, string key, bool encrypt)
+        {
+            if (string.IsNullOrEmpty(text) || string.IsNullOrEmpty(key))
+                throw new ArgumentException("Текст или ключ не могут быть пустыми!");
+
+            string selectedLanguage = comboBox1.SelectedItem.ToString();
+            string alphabet;
+            if (selectedLanguage == "Русский")
+            {
+                alphabet = RussianAlphabet;
+            }
+            else
+            {
+                alphabet = EnglishAlphabet;
+            }
+
+            ValidateInput(text, key, alphabet, selectedLanguage);
+
+            key = key.Replace(" ", "").ToLower();
+            var result = new StringBuilder();
+            int keyPosition = 0;
+
+            foreach (char symbol in text.ToLower())
+            {
+                int symbolIndex = alphabet.IndexOf(symbol);
+                if (symbolIndex == -1)
+                    throw new ArgumentException("Недопустимый символ");
+
+                int keyShift = alphabet.IndexOf(key[keyPosition % key.Length]);
+                int shift = 0;
+                if (encrypt)
+                {
+                    shift = keyShift;
+                }
+                else
+                {
+                    shift = -keyShift;
+                }
+
+                int newIndex = (symbolIndex + shift + alphabet.Length) % alphabet.Length;
+                result.Append(alphabet[newIndex]);
+                keyPosition++;
+            }
+
+            return result.ToString();
+        }
+
+        private void ValidateInput(string text, string key, string alphabet, string selectedLanguage)
+        {
+            foreach (char c in key)
+            {
+                if (!alphabet.Contains(c))
+                    throw new ArgumentException("Ключ содержит недопустимый символ");
+            }
+
+            bool hasRussian = text.Any(c => RussianLetters.Contains(c));
+            bool hasEnglish = text.Any(c => EnglishLetters.Contains(c));
+
+            if (hasRussian && selectedLanguage != "Русский")
+                throw new ArgumentException("Текст содержит русские буквы, но выбран английский алфавит!");
+
+            if (hasEnglish && selectedLanguage != "Английский")
+                throw new ArgumentException("Текст содержит английские буквы, но выбран русский алфавит!");
+
+            foreach (char c in text)
+            {
+                if (!alphabet.Contains(c))
+                    throw new ArgumentException("Текст содержит недопустимый символ");
+            }
+        }
+
+        private void ProcessButtonClick(bool encrypt)
+        {
+            try
+            {
+                string text = txtInput.Text;
+                string key = tKey.Text;
+
+                if (string.IsNullOrWhiteSpace(key))
+                    throw new ArgumentException("Введите ключ!");
+
+                txtOutput.Text = ProcessVigenere(text, key, encrypt);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void enc_Click(object sender, EventArgs e) => ProcessButtonClick(encrypt: true);
+        private void dec_Click(object sender, EventArgs e) => ProcessButtonClick(encrypt: false);
+    }
+}
